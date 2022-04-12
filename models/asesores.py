4ee495cdad1a7f6931608bcd07b9911395368bf6
Erasto Flores <erasto.flores@amats.com.mx>
2022-04-12 12:17:26 -0500

    de models a asesores

---
+++ models/asesores.py
@@ -0,0 +1,42 @@
+from odoo import models, fields, api
+
+
+class Asesorcomponents(models.Model):
+    _name = 'module_test.asesor'
+    _description = 'Componentes de Asesor'
+
+    asesor = fields.Many2one('hr.employee', 'Asesor', attribute='id')
+    currency_id = fields.Many2one('res.currency', 'Moneda', required=True, writeonly=False)
+    puesto = fields.Char(related='asesor.job_title', string='Puesto', store=True)
+
+
+class Rules_total_asesor_ventas(models.Model):
+    _name = 'module_test.rules_total_asesor'
+
+    rule_total_id = fields.Integer()
+    year = fields.Integer(string='Año')
+    currency_id = fields.Many2one('res.currency', string='Moneda')
+    meta_anual = fields.Monetary(string='🎯Meta Anual', computed='_get_meta_anual', store=True)
+    meta_mes = fields.One2many('module_test.rules_mes_asesor', 'rule_mes_id', string='🎯 Meta de meses', store=True)
+
+    @api.depends('meta_mes')
+    def _get_meta_anual(self):
+        for record in self:
+            record.meta_anual = sum(line.meta for line in record.meta_mes)
+
+
+class Rules_mes_asesor_ventas(models.Model):
+    _name = 'module_test.rules_mes_asesor'
+
+    sequence = fields.Integer()
+
+    rule_mes_id = fields.Integer()
+    rule_anual_id = fields.Many2one(inverse_name='meta_mes', store=True)
+    fecha_inicio = fields.Date(string='Fecha Inicio')
+    fecha_fin = fields.Date(string='Fecha Fin')
+    currency_id = fields.Many2one('res.currency', string='Moneda')
+    nombre_mes = fields.Char(string='Nombre del Mes')
+    meta = fields.Monetary(string='Meta')
+
+
+
