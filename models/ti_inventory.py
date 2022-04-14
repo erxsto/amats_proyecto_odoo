b94ced156ee5f059f14a5fee58f2140fc48036eb
Erasto Flores <erasto.flores@amats.com.mx>
2022-04-14 13:54:17 -0500

    brand

--- models/ti_inventory.py
+++ models/ti_inventory.py
@@ -6,13 +6,22 @@ class Dispositivo(models.Model):
     _description = 'dispositivos electronicos'
 
     tipo = fields.Selection([('c', 'computadora'), ('t', 'telefono')], default='c')
-    marca = fields.Many2one('module_test.app', string = 'marca')
-    
+    marca = fields.Many2one('module_test.brand', string = 'marca')
+    app = fields.One2many('module_test.app','id_app',string = 'aplicacion')
 
 class Aplicacion(models.Model):
     _name = 'module_test.app'
     _description = 'software'
 
+    id_app = fields.Integer()
+    sequence = fields.Integer()
     name = fields.Char(string = 'nombre')
     descripcion = fields.Char(string = 'descripcion')
     version = fields.Char(string = 'version')
+
+    
+class Marca(models.Model):
+    _name = 'module_test.brand'
+    _description = 'Marca de dispositivos'
+
+    name = fields.Char(string = 'nombre')
